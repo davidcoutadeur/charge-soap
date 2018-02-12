3f4b0a900cd4fae81320ee6b2bd196ae44911232
davidcoutadeur <david.coutadeur@gmail.com>
2018-02-12 18:27:39 +0100

    improve limit increasement

--- increase-limit.sh
+++ increase-limit.sh
@@ -1,10 +1,48 @@
-#!/bin/bash -x
+#!/bin/bash
+
+VALUE="1048576"
+
+#########################
+# Changing values
+#########################
 
 # maximum system threads
-echo 1000000 > /proc/sys/kernel/threads-max
+echo "$VALUE" > /proc/sys/kernel/threads-max
+
+# maximum number of contrack connexions
+echo "$VALUE" > /proc/sys/net/ipv4/ip_conntrack_max
 
 # maximum open files
-ulimit -n 1048576
+ulimit -n $VALUE
 
 # maximum user processes
-ulimit -u 1048576
+ulimit -u $VALUE
+
+
+#########################
+# Verifications
+#########################
+
+echo "maximum system threads"
+echo -n "value:    "
+cat /proc/sys/kernel/threads-max
+echo "expected: $VALUE"
+echo
+
+echo "maximum number of contrack connexions"
+echo -n "value:    "
+cat /proc/sys/net/ipv4/ip_conntrack_max
+echo "expected: $VALUE"
+echo
+
+echo "maximum open files"
+echo -n "value:    "
+ulimit -n
+echo "expected: $VALUE"
+echo
+
+echo "maximum user processes"
+echo -n "value:    "
+ulimit -u
+echo "expected: $VALUE"
+echo
